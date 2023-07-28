bf433be205b2f99bc2602e33a28930f15c8505eb
Christian <chrisnebech@hotmail.com>
2023-07-27 23:19:24 -0300

    requisito 5

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -2,6 +2,9 @@ package com.betrybe.sistemadevotacao;
 
 import java.util.ArrayList;
 
+/**
+ * Define a classe GerenciamentoVotacao.
+ */
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
@@ -10,12 +13,26 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-
+    for (PessoaCandidata pessoa : pessoasCandidatas) {
+      if (pessoa.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    PessoaCandidata newPerson = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(newPerson);
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-
+    for (PessoaEleitora pessoa : pessoasEleitoras) {
+      if (pessoa.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    PessoaEleitora newPerson = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(newPerson);
   }
 
   @Override
